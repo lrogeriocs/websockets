5fb01f2111ebdad7f09cf9a84b2a40f390277e13
lrogeriocs <lrogeriocsantos@gmail.com>
2018-11-28 16:31:17 +0000

    chat using socket.io

---
+++ chat/chat.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io');
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000, ()=>console.log('listing 9000 port'));
+const io = socketio(expressServer);
+
+io.on('connection', (socket)=>{
+     socket.emit('messageFromServer', 'Welcome to the socketio server');
+     socket.on('dataToServer', (dataFromClient)=>{
+         console.log(dataFromClient);
+     })
+     socket.on('newMessageToServer', (message)=>{
+         //console.log(message);
+         io.emit('messageToClients', {text: message.text});
+     })
+ })
